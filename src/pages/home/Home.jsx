836040319cfb417bc15240280444b7cb475afdff
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-19 23:05:03 +0300

    included all the sections of home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,32 @@
+import About from "../../components/about/About";
+import Contact from "../../components/contact/Contact";
+import Dropdown from "../../components/dropdown/Dropdown";
+import Hero from "../../components/hero/Hero";
+import Navbar from "../../components/navbar/Navbar";
+import Portfolio from "../../components/portfolio/Portfolio";
+import Pricing from "../../components/pricing/Pricing";
+import Services from "../../components/services/Services";
+import Team from "../../components/team/Team";
+
 function Home() {
-  return <div>home</div>;
+  return (
+    <main>
+      <nav>
+        <Navbar />
+      </nav>
+
+      <section>
+        <Hero />
+        <About />
+        {/* <Contact />
+        <Dropdown />
+        <Portfolio />
+        <Pricing />
+        <Services />
+        <Team /> */}
+      </section>
+    </main>
+  );
 }
 
 export default Home;
