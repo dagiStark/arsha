0bc396254658adfdc1f5827554431c6226fc6817
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-19 23:04:27 +0300

    installed required dependencies and cleaned the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import { Button } from "@mui/material";
-import "./App.css";
+import Home from "./pages/home/Home";
+import "./App.css"
 
 function App() {
   return (
     <>
-      <p className="text-2xl text-blue-500">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button
-        sx={{
-          backgroundColor: "#0070f3",
-          color: "white",
-          borderRadius: "5px",
-          padding: "10px",
-          margin: "10px",
-          fontSize: "16px",
-          cursor: "pointer",
-        }}
-      >
-        Learn More
-      </Button>
+      <div>
+        <Home />
+      </div>
     </>
   );
 }
