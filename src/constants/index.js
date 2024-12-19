07862443937b005d4bc1b7162fa652d819644e1d
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-19 23:05:18 +0300

    added constants

---
+++ src/constants/index.js
@@ -0,0 +1,64 @@
+export const navbarLinks = [
+  {
+    name: "Home",
+    path: "hero",
+  },
+  {
+    name: "About",
+    path: "about",
+  },
+  {
+    name: "Services",
+    path: "services",
+  },
+  {
+    name: "Portfolio",
+    path: "portfolio",
+  },
+  {
+    name: "Team",
+    path: "team",
+  },
+  {
+    name: "Pricing",
+    path: "pricing",
+  },
+  {
+    name: "Contact",
+    path: "contact",
+  },
+];
+
+export const aboutItems = [
+  {
+    id: 1,
+    description: "We are a team of talented individuals  ",
+  },
+  {
+    id: 2,
+    description: "We are a team of talented individuals  ",
+  },
+  {
+    id: 3,
+    description: "We are a team of talented individuals",
+  },
+];
+
+export const skillItems = [
+  {
+    name: "HTML",
+    percentage: 90,
+  },
+  {
+    name: "CSS",
+    percentage: 70,
+  },
+  {
+    name: "JavaScript",
+    percentage: 80,
+  },
+  {
+    name: "PhotoShop",
+    percentage: 60,
+  },
+];
