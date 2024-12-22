8c37c708d6c357af7fa6804d903bb3cb4dc5ee11
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-22 20:53:42 +0300

    created map

---
+++ src/components/contact/MapComponent.jsx
@@ -0,0 +1,11 @@
+function MapComponent() {
+  return (
+    <iframe
+      width="100%"
+      height="400"
+      src="https://www.openstreetmap.org/export/embed.html?bbox=-2.0%2C52.0%2C2.0%2C54.0&layer=mapnik"
+    ></iframe>
+  );
+}
+
+export default MapComponent;
