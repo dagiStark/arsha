28ecaa6438407043d23aa8049c62ea60aae18ef8
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-22 20:54:18 +0300

    added constants required

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,25 @@
+import { Activity, Scan, Album, Radio } from "lucide-react";
+import portfolio1 from "../assets/img/masonry-portfolio/masonry-portfolio-1.jpg";
+import portfolio2 from "../assets/img/masonry-portfolio/masonry-portfolio-2.jpg";
+import portfolio3 from "../assets/img/masonry-portfolio/masonry-portfolio-3.jpg";
+import portfolio4 from "../assets/img/masonry-portfolio/masonry-portfolio-4.jpg";
+import portfolio5 from "../assets/img/masonry-portfolio/masonry-portfolio-5.jpg";
+import portfolio6 from "../assets/img/masonry-portfolio/masonry-portfolio-6.jpg";
+import portfolio7 from "../assets/img/masonry-portfolio/masonry-portfolio-7.jpg";
+import portfolio8 from "../assets/img/masonry-portfolio/masonry-portfolio-8.jpg";
+import portfolio9 from "../assets/img/masonry-portfolio/masonry-portfolio-9.jpg";
+
+import team1 from "../assets/img/team/team-1.jpg";
+import team2 from "../assets/img/team/team-2.jpg";
+import team3 from "../assets/img/team/team-3.jpg";
+import team4 from "../assets/img/team/team-4.jpg";
+
+import testimonial1 from "../assets/img/testimonials/testimonials-1.jpg";
+import testimonial2 from "../assets/img/testimonials/testimonials-2.jpg";
+import testimonial3 from "../assets/img/testimonials/testimonials-3.jpg";
+import testimonial4 from "../assets/img/testimonials/testimonials-4.jpg";
+import testimonial5 from "../assets/img/testimonials/testimonials-5.jpg";
+
 export const navbarLinks = [
   {
     name: "Home",
@@ -62,3 +84,247 @@ export const skillItems = [
     percentage: 60,
   },
 ];
+
+export const serviceItems = [
+  {
+    title: "Web Design",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+    icon: Activity,
+  },
+  {
+    title: "Web Development",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+    icon: Album,
+  },
+  {
+    title: "BOT Development",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+    icon: Radio,
+  },
+  {
+    title: "UI/UX Design",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+    icon: Scan,
+  },
+];
+
+export const portfolioItems = [
+  {
+    img: portfolio1,
+    category: "app",
+  },
+  {
+    img: portfolio2,
+    category: "card",
+  },
+  {
+    img: portfolio3,
+    category: "web",
+  },
+
+  {
+    img: portfolio4,
+    category: "app",
+  },
+  {
+    img: portfolio5,
+    category: "card",
+  },
+  {
+    img: portfolio6,
+    category: "web",
+  },
+  {
+    img: portfolio7,
+    category: "app",
+  },
+  {
+    img: portfolio8,
+    category: "card",
+  },
+  {
+    img: portfolio9,
+    category: "web",
+  },
+];
+
+export const teamMembers = [
+  {
+    img: team1,
+    name: "John Doe",
+    designation: "CEO",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+  },
+  {
+    img: team2,
+    name: "John Doe",
+    designation: "CEO",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+  },
+  {
+    img: team3,
+    name: "John Doe",
+    designation: "CEO",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+  },
+  {
+    img: team4,
+    name: "John Doe",
+    designation: "CEO",
+    description: "Lorem ipsum dolor sit amet consectetur ",
+  },
+];
+
+export const pricingCards = [
+  {
+    title: "Basic",
+    price: 0,
+    description: "Lorem ipsum dolor sit amet consectetur ",
+    features: [
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+    ],
+  },
+  {
+    title: "Pro",
+    price: 29,
+    description: "Lorem ipsum dolor sit amet consectetur ",
+    features: [
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: 49,
+    description: "Lorem ipsum dolor sit amet consectetur ",
+    features: [
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+      "Lorem ipsum dolor sit amet consectetur ",
+    ],
+  },
+];
+
+export const testimonialItems = [
+  {
+    id: 1,
+    name: "John Doe",
+    designation: "Co-Founder & CEO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur ",
+    img: testimonial1,
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Jena Karlis",
+    designation: "Store Owner",
+    description:
+      "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur ",
+    img: testimonial2,
+    rating: 5,
+  },
+  {
+    id: 3,
+    name: "Sarah Wilsson",
+    designation: "HR",
+    description:
+      "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur ",
+    img: testimonial3,
+    rating: 5,
+  },
+  {
+    id: 4,
+    name: "Matt Brandon",
+    designation: "Freelancer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur ",
+    img: testimonial4,
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: "John Larsor",
+    designation: "Entrepreneur",
+    description:
+      "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur ",
+    img: testimonial5,
+    rating: 5,
+  },
+];
+
+export const frequentQuestions = [
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni?",
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni?",
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni?",
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni?",
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, magni?",
+  },
+];
+
+export const contactLinks = {
+  usefulLinks: [
+    {
+      label: "Home",
+      path: "/",
+    },
+    {
+      label: "About Us",
+      path: "/",
+    },
+    {
+      label: "Services",
+      path: "/",
+    },
+    {
+      label: "Terms of Service",
+      path: "/",
+    },
+  ],
+  serviceLinks: [
+    {
+      label: "Web Design",
+      path: "/",
+    },
+    {
+      label: "Web Development",
+      path: "/",
+    },
+    {
+      label: "Product Management",
+      path: "/",
+    },
+    {
+      label: "Marketing",
+      path: "/",
+    },
+  ],
+};
